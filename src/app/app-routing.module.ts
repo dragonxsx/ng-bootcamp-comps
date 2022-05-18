42955d21e17633f37aaf8bda028662ce911ad3e3
Long Nguyen <long_nguyen@epam.com>
2022-05-18 15:18:38 +0700

    Using Router and routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
+import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'elements', component: ElementsHomeComponent },
+  { path: 'collections', component: CollectionsHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
