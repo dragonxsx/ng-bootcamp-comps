68b3f533c0fcb74da070aceabc706c0c4c5d9d05
Long Nguyen <long_nguyen@epam.com>
2022-05-20 14:22:26 +0700

    Add shared module - divider component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,6 +5,7 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { TimesDirective } from './times.directive';
   imports: [
     CommonModule,
     ElementsRoutingModule
+    SharedModule
   ]
 })
 export class ElementsModule { }
